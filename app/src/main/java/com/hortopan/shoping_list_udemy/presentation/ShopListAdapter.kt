fba4313d8fe3310af4953f2cb423f18f84dcfbec
and-reyg <mr.ansegor@gmail.com>
2022-12-20 12:20:13 +0200

    part RecyclerView lesson 16. Bugs

--- app/src/main/java/com/hortopan/shoping_list_udemy/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/hortopan/shoping_list_udemy/presentation/ShopListAdapter.kt
@@ -5,19 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.hortopan.shoping_list_udemy.R
 import com.hortopan.shoping_list_udemy.domain.ShopItem
 
 class ShopListAdapter() : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(){
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     //реализует как создать View
     //будет вызван всего условно 20 раз (вместо всех условных 10 000 раз по условно 10 000 елементов)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         //создаем view из макета
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
         //возвращаем созданый обьект ShopItemViewHolder
         return ShopItemViewHolder(view)
     }
@@ -26,19 +35,36 @@ class ShopListAdapter() : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolde
     //будет вызван для абсолютно всех элементов списка
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         //получаем элемент из коллекции
-        val shopItem = list[position]
-        //у полученых TextView устанавливаем значения
-        viewHolder.tvName.text = shopItem.name
-        viewHolder.tvCount.text = shopItem.count.toString()
+        val shopItem = shopList[position]
+        val status = if(shopItem.enabled){
+            "Active"
+        }else{
+            "Not active"
+        }
+
         //устанавливаем слушатель нажатия (клика)
         viewHolder.view.setOnClickListener{
             true
         }
+        if(shopItem.enabled){
+            //у полученых TextView устанавливаем значения
+            viewHolder.tvName.text = "${shopItem.name} $status"
+            viewHolder.tvCount.text = shopItem.count.toString()
+            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_light))
+        }
+    }
+
+    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
+        super.onViewRecycled(viewHolder)
+        viewHolder.tvName.text = ""
+        viewHolder.tvCount.text = ""
+        viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.white))
+
     }
 
     //возвращает количество елементов в колекции
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 
     // класс который хранит View
