89479e5ac9c8cf09b7c8bdc3754da6bfcb29f3ea
and-reyg <mr.ansegor@gmail.com>
2022-12-21 01:21:46 +0200

    part RecyclerView lesson 18. Add Listeners

--- app/src/main/java/com/hortopan/shoping_list_udemy/presentation/MainActivity.kt
+++ app/src/main/java/com/hortopan/shoping_list_udemy/presentation/MainActivity.kt
@@ -3,18 +3,17 @@ package com.hortopan.shoping_list_udemy.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.hortopan.shoping_list_udemy.R
 import com.hortopan.shoping_list_udemy.domain.ShopItem
+import com.hortopan.shoping_list_udemy.presentation.ShopListAdapter.Companion as ShopListAdapter1
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +23,71 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java] // или точно такое же .get(MainViewModel::class.java)
         viewModel.shopList.observe(this){
             //присвоить адаптеру новый list
-            adapter.shopList = it
+            shopListAdapter.shopList = it
         }
     }
 
     private fun setupRecyclerView(){
         //получить RecyclerView
         val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
-        //создать адаптер
-        adapter = ShopListAdapter()
-        //установить этот адаптер к RecyclerView
-        rvShopList.adapter = adapter
+        with(rvShopList){
+            //создать адаптер
+            shopListAdapter = ShopListAdapter()
+            //установить этот адаптер к RecyclerView
+            adapter = shopListAdapter
+            //Установка размера ViewHolderPool
+            // setMaxRecycledViews принимает 2 параметра View  и размер пула, так как в проекте 2 типа View нужно написать дважды
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE)
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE)
+        }
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rvShopList)
+
     }
+
+    private fun setupClickListener() {
+        shopListAdapter.onShopItemClickListener = {
+            Log.d("MainActivity", it.toString())
+        }
+    }
+
+    private fun setupLongClickListener() {
+        shopListAdapter.onShopItemLongClickListener = {
+            viewModel.changeEnableState(it)
+        }
+    }
+
+    private fun setupSwipeListener(rvShopList: RecyclerView){
+        //создание анонимного обьекта
+        //SimpleCallback(направление перемещения и свайпа
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                //этот метод не нужен поэтому просто так написали false
+                return false
+            }
+            //удвление элемента их списка, ведь при свайпе удалится жлемент
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = shopListAdapter.shopList[viewHolder.adapterPosition]
+                viewModel.deleteShopItem(item)
+            }
+        }
+        //передача анонимного обьекта и создание itemTouchHelper
+        val itemTouchHelper = ItemTouchHelper(callback)
+        //присваивание к ресайклеру
+        itemTouchHelper.attachToRecyclerView(rvShopList)
+    }
+
 
 }
--- app/src/main/java/com/hortopan/shoping_list_udemy/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/hortopan/shoping_list_udemy/presentation/ShopListAdapter.kt
@@ -18,15 +18,22 @@ class ShopListAdapter() : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolde
             notifyDataSetChanged()
         }
 
+    //лямбда интерфейс, принимает ShopItem, ничего не возвращает, имеет тип null
+    var onShopItemLongClickListener: ((ShopItem) -> Unit) ? = null
+
+    var onShopItemClickListener: ((ShopItem) -> Unit) ? = null
+
     //реализует как создать View
     //будет вызван всего условно 20 раз (вместо всех условных 10 000 раз по условно 10 000 елементов)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        //обработка нужного ViewType
+        var layout = when(viewType){
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            else -> throw RuntimeException("Unknown viewType: $viewType")
+        }
         //создаем view из макета
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_shop_disabled,
-            parent,
-            false
-        )
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         //возвращаем созданый обьект ShopItemViewHolder
         return ShopItemViewHolder(view)
     }
@@ -36,22 +43,18 @@ class ShopListAdapter() : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolde
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         //получаем элемент из коллекции
         val shopItem = shopList[position]
-        val status = if(shopItem.enabled){
-            "Active"
-        }else{
-            "Not active"
-        }
-
         //устанавливаем слушатель нажатия (клика)
         viewHolder.view.setOnClickListener{
+            onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-        if(shopItem.enabled){
-            //у полученых TextView устанавливаем значения
-            viewHolder.tvName.text = "${shopItem.name} $status"
-            viewHolder.tvCount.text = shopItem.count.toString()
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_light))
+        viewHolder.view.setOnClickListener{
+            onShopItemClickListener?.invoke(shopItem)
         }
+        //у полученых TextView устанавливаем значения
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+
     }
 
     override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
@@ -67,9 +70,36 @@ class ShopListAdapter() : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolde
         return shopList.size
     }
 
+    //отвечает за определения типа view
+    //вернет позицию элемента (у 0 элемента ViewType = 0, у 1 элемента ViewType = 1 и тд)
+    override fun getItemViewType(position: Int): Int {
+        //получение элемента
+        val item = shopList[position]
+        return if(item.enabled){
+            VIEW_TYPE_ENABLED
+        }else{
+            VIEW_TYPE_DISABLED
+        }
+
+    }
+
     // класс который хранит View
     class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    interface OnShopItemLongClickListener{
+
+        fun onShopItemLongClick(shopItem: ShopItem)
+    }
+
+    companion object{
+
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+
+        //количество ViewHolder
+        const val MAX_POOL_SIZE = 15
     }
 }
