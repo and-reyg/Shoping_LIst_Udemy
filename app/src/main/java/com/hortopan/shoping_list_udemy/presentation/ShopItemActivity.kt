4c55580dfe4332d6b749feb31ce35167efd02e64
and-reyg <mr.ansegor@gmail.com>
2022-12-21 19:44:53 +0200

    part Second Screen lesson 23.  - Add New Activity  - Add New MAket

---
+++ app/src/main/java/com/hortopan/shoping_list_udemy/presentation/ShopItemActivity.kt
@@ -0,0 +1,15 @@
+package com.hortopan.shoping_list_udemy.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hortopan.shoping_list_udemy.R
+
+class ShopItemActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+
+}
